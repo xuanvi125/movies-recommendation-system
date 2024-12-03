dde133da8657c686090cb9ea376e29df6d32b657
xuanvi <nguyenxuanvi770@gmail.com>
2024-12-03 22:05:12 +0700

    add full name field to registration form

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -52,6 +52,33 @@ function SignUp() {
                   htmlFor="email"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  {...register("name", {
+                    required: "This field is required",
+                    minLength: {
+                      value: 6,
+                      message: "Minimum length should be 6",
+                    },
+                  })}
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="John Smith"
+                  required
+                />
+                {errors?.name && (
+                  <Typography color="red" className="mt-1">
+                    {errors.name.message}
+                  </Typography>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
                   Email
                 </label>
                 <input
