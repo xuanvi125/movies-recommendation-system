cbe3208d2b3f051457ea3cc46e45328d81ef9ad1
nhat.ntm <nhat.ntm@teko.vn>
2024-12-11 08:54:44 +0700

    Remove strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -6,11 +5,9 @@ import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HashRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </HashRouter>
-  </StrictMode>
+  <HashRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </HashRouter>
 );
